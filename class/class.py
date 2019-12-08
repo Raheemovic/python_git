da4af4f539eb1e58032a6fd9e49fbef03891848d
Raheem Abioye <raheem.abioye@yahoo.ca>
2019-12-08 15:31:15 -0500

    Class

---
+++ class/class.py
@@ -0,0 +1,10 @@
+class car:
+    def __init__(self, horn, steering_wheel, tires, door, seats, ignition, brake):
+        self.horn = horn
+        self.steering_wheel = steering_wheel
+        self.tires = tires
+        self.door = door
+        self.seats = seats
+        self.ignition = ignition
+        self.brake = brake
+        
